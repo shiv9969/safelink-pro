b8580997cbac8fb99a8de672830685a188edaa42
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2026-03-21 14:12:59 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,14 @@
+# 🔐 SECRET
+SECRET_KEY = "your_secret"
+
+# 🔑 CAPTCHA
+RECAPTCHA_SITE_KEY = "your_site_key"
+RECAPTCHA_SECRET_KEY = "your_secret_key"
+
+# 🌐 BASE URL
+BASE_URL = "https://safe-url-ff28643cb1cc.herokuapp.com/"
+
+# 🍃 MONGODB
+MONGO_URI = "your_mongodb_connection_string"
+DB_NAME = "safelink"
+COLLECTION_NAME = "links"

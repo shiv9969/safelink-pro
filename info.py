3a0ae7b3ed80aa22b2e4c6d57bbee59839a0bad0
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2026-05-26 19:26:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -6,7 +6,7 @@ RECAPTCHA_SITE_KEY = "6LddGJIsAAAAAEIi29EUEhu_21LQJbeuhoNL0raK"
 RECAPTCHA_SECRET_KEY = "6LddGJIsAAAAANbJmudv0hJ_NAF97OFbnWeTouAc"
 
 # 🌐 BASE URL
-BASE_URL = "https://safe-url-ff28643cb1cc.herokuapp.com/"
+BASE_URL = "https://safe-link-68f3bac4b782.herokuapp.com/"
 
 # 🍃 MONGODB
 MONGO_URI = "mongodb+srv://shiv:mishraji@cluster0.80smvxk.mongodb.net/?appName=Cluster0"
